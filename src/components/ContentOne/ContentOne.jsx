90d38fb7c6ce1692b6141d2cec20dc113e3d8f25
raihanhn <raihanhn20@gmail.com>
2022-11-06 05:50:48 +0600

    7th

--- src/components/ContentOne/ContentOne.jsx
+++ src/components/ContentOne/ContentOne.jsx
@@ -4,6 +4,7 @@ import img from '../../assets/img/edu hack.jpg'
 const ContentOne = () => {
   return (
     <div className="cone">
+        <div className="kill"></div>
         <h1>OUR SINCERE PLEDGE TO ALL STUDENTS</h1>
         <img src={img} alt="" />
     </div>
