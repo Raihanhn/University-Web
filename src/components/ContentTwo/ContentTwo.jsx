ff01251d32ec1fa0f35aaccac3116730f18deacf
raihanhn <raihanhn20@gmail.com>
2022-11-07 06:12:41 +0600

    12th

--- src/components/ContentTwo/ContentTwo.jsx
+++ src/components/ContentTwo/ContentTwo.jsx
@@ -12,10 +12,22 @@ const ContentTwo = () => {
         <h1 className='rokis'>PROGRAMS</h1>
         </div>
 
+        <div className="ver">
+          <ul>
+            <li>▪ &nbsp; BACHELOR OF BUSINESS ADMINISTRATION <span>(BBA)</span></li>
+            <li>▪ &nbsp; BACHELOR OF SCIENCE IN COMPUTER SCIENCE & ENGINEERING <span>(B.SC. IN CSE)</span> </li>
+            <li>▪ &nbsp; BACHELOR OF SCIENCE IN ELECTRICAL & ELECTRONIC ENGINEERING <span>(B.SC IN EEE)</span> </li>
+            <li>▪ &nbsp; BACHELOR OF SCIENCE IN ELECTRONICS & TELECOMMUNICATION ENGINEERING <span>(B.SC IN ETE)</span></li>
+            <li>▪ &nbsp; BACHELOR OF ARTS <span>(BA) IN ENGLISH</span></li>
+            <li>▪ &nbsp; BACHELOR OF SCIENCE <span>(BSC) IN ECONOMICS</span></li>
+          </ul>
+        </div>
+
         <div className="akosi">
         <h1>POSTGRADUATE</h1>
         <h1 className='rokisi'>PROGRAMS</h1>
         </div>
+
 
     </div>
   )
