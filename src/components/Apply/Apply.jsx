a763b5aa4b6303966c8f63fdf441df19ec40f2e0
raihanhn <raihanhn20@gmail.com>
2022-11-15 23:37:32 +0600

    18th

--- src/components/Apply/Apply.jsx
+++ src/components/Apply/Apply.jsx
@@ -4,6 +4,7 @@ const Apply = () => {
   return (
     <div className="apply">
          <p>Join us and you can be part of an institution that<br/> has an ongoing and dynamic commitment<br/> towards</p>
+         <p className='tokio'> “Advancing Knowledge, Enriching Lives”</p>
         <div className="motion">
         <iframe width="560" height="315" src="https://www.youtube.com/embed/LIaAvZceY6c" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
