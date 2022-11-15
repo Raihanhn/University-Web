a59db166178d8c61b76580770b5b797cf683e32c
raihanhn <raihanhn20@gmail.com>
2022-11-15 23:08:42 +0600

    17th

--- src/components/Apply/Apply.jsx
+++ src/components/Apply/Apply.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 const Apply = () => {
   return (
     <div className="apply">
-        Raihan is Back !
+         <p>Join us and you can be part of an institution that<br/> has an ongoing and dynamic commitment<br/> towards</p>
+        <div className="motion">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/LIaAvZceY6c" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
     </div>
   )
 }
