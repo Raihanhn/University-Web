77ca23cc2b99c7fc24683f2fe5707c3cd407d437
raihanhn <raihanhn20@gmail.com>
2022-11-16 16:51:44 +0600

    21th

--- src/components/Links/Links.jsx
+++ src/components/Links/Links.jsx
@@ -6,6 +6,19 @@ const Links = () => {
         <div className="pin">
             <p className='ney'>Important Links</p>
         </div>
+
+        <div className="resource">
+          yo
+        </div>
+
+        <div className="inter">
+          <div className="bot">
+            <button className='tera'>Application/proposal for "International Mother Language International Award 2023"</button><br/>
+            <button className='terao'>Application/proposal for "International Mother Language International Award 2023"</button><br/>
+            <button className='terai'>Osmosis: Second International Conference by the department of English</button><br/>
+            <button className='teras'>Bus Schedule</button>
+          </div>
+        </div>
     </div>
   )
 }
