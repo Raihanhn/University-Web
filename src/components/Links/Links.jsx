a5425d49d0f0d7927f48cf037d5fb36f5ebf9e23
raihanhn <raihanhn20@gmail.com>
2022-11-17 15:26:29 +0600

    23th

--- src/components/Links/Links.jsx
+++ src/components/Links/Links.jsx
@@ -9,6 +9,38 @@ const Links = () => {
 
         <div className="resource">
           <button className="bet">RESOURCES FOR FACULTY & ADMIN</button>
+          <div className="firstly">
+          <a className='marke'>▪ <br/> ▪ <br/>▪ <br/> ▪ <br/>▪</a>
+            <ul>
+              <li><a href="#">Apply Online</a></li>
+              <li><a href="#">Student Code of Conduct</a></li>
+              <li><a href="#">Center for Professional Development and Change</a></li>
+              <li><a href="#">Career Opportunities</a></li>
+              <li><a href="#">Scholarship Repository</a></li>
+            </ul>
+          </div>
+
+          <div className="secondly">
+          <a className='markes'>▪ <br/> ▪ <br/>▪ <br/> ▪ <br/>▪</a>
+            <ul>
+              <li><a href="#">UGC, Bangladesh</a></li>
+              <li><a href="#">Ministry of Education, Bangladesh</a></li>
+              <li><a href="#">Apply for Degree Certificate</a></li>
+              <li><a href="#">Degree Verification System</a></li>
+              <li><a href="#">Academic Support Clinic </a></li>
+            </ul>
+          </div>
+
+          <div className="thirdly">
+          <a className='markew'>▪ <br/> ▪ <br/>▪ <br/> ▪ <br/>▪</a>
+            <ul>
+              <li><a href="#">COVID-19 Update</a></li>
+              <li><a href="#">EDU Profile</a></li>
+              <li><a href="#"><span className='redd'>ePayment Portal</span></a></li>
+              <li><a href="#">EDU Blackbox</a></li>
+              <li><a href="#">Google Workspace Account</a></li>
+            </ul>
+          </div>
         </div>
 
         <div className="inter">
